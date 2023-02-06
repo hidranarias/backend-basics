9a260a3e20510a13cd808fa2526a4c667d020884
Hidran <hidran.arias@spryker.com>
2023-02-06 18:10:13 +0100

    Adds data transfer object for antelopes

--- src/Pyz/Zed/Training/Communication/Controller/HelloController.php
+++ src/Pyz/Zed/Training/Communication/Controller/HelloController.php
@@ -2,12 +2,19 @@
 
 namespace Pyz\Zed\Training\Communication\Controller;
 
+use Generated\Shared\Transfer\AntelopeTransfer;
 use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
 
 class HelloController extends AbstractController
 {
     public function indexAction()
     {
+        $antelopeTransfer = new AntelopeTransfer();
+        $antelopeTransfer->setName('Oskar');
+
+        return $this->viewResponse([
+            'antelope' => $antelopeTransfer
+        ]);
         return $this->viewResponse(['helloWorldText' => 'Hello world']);
     }
 }
