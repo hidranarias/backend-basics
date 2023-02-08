41f2df002eb693bfa0c399e07883b87146a8c71c
Hidran <hidran.arias@spryker.com>
2023-02-08 01:08:03 +0100

    Adds customer form to factory

---
+++ src/Pyz/Yves/CustomerPage/Form/ProfileForm.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Pyz\Yves\CustomerPage\Form;
+
+use Pyz\Yves\CustomerPage\CustomerPageFactory;
+use SprykerShop\Yves\CustomerPage\Form\ProfileForm as SprykerProfileForm;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * @method CustomerPageFactory getFactory()
+ */
+class ProfileForm extends SprykerProfileForm
+{
+    public const FIELD_ANTELOPE = 'fk_antelope';
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        parent::buildForm($builder, $options);
+        $this->addAntelopeField($builder);
+    }
+
+    public function addAntelopeField(FormBuilderInterface $builder): ProfileForm
+    {
+        $builder->add(self::FIELD_ANTELOPE, TextType::class, [
+            'label' => 'customer.profile.antelope',
+            'required' => true,
+            'constraints' => [
+                $this->createNotBlankConstraint(),
+            ],
+        ]);
+
+        $builder->get(self::FIELD_ANTELOPE)->addModelTransformer(
+            $this->getFactory()->createAntelopeTransformer()
+        );
+
+        return $this;
+    }
+}
